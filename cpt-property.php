614f1867626ad5afd044d225c2be638a4a1361c3
Vineet Tiwari <166735997+vineettiwari1708@users.noreply.github.com>
2025-12-17 13:06:18 +0530

    Update cpt-property.php

--- cpt-property.php
+++ cpt-property.php
@@ -1,30 +1,17 @@
 <?php
-// Prevent direct access
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-/**
- * Register Property CPT
- */
 function simple_git_property_register_cpt() {
-
-    $labels = array(
-        'name'               => 'Properties',
-        'singular_name'      => 'Property',
-        'menu_name'          => 'Properties',
-        'name_admin_bar'     => 'Property',
-        'add_new_item'       => 'Add New Property',
-        'edit_item'          => 'Edit Property',
-    );
-
-    $args = array(
-        'labels'             => $labels,
-        'public'             => true,
-        'has_archive'        => true,
-        'rewrite'            => array( 'slug' => 'properties' ),
-        'supports'           => array( 'title', 'editor', 'thumbnail' ),
-        'show_in_rest'       => true,
-    );
-
-    register_post_type( 'property', $args );
+    register_post_type('property', array(
+        'labels' => array(
+            'name' => 'Properties',
+            'singular_name' => 'Property'
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'properties'),
+        'supports' => array('title','editor','thumbnail'),
+        'show_in_rest' => true,
+    ));
 }
-add_action( 'init', 'simple_git_property_register_cpt' );
+add_action('init', 'simple_git_property_register_cpt');
